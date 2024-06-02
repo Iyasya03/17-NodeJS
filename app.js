9c85382e9262511f74efe4ac44bd5b7e21590251
Iyasya03 <ahiyasya@gmail.com>
2024-06-02 11:02:36 +0700

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const { loadContact, findContact } = require('./utils/contacts');
+const { loadContact, findContact, addContact, cekDuplikat } = require('./utils/contacts');
+const {body, validationResult, check, cookie} = require('express-validator')
+const session = require('express-session')
+const cookeiParser = require('cookie-parser')
+const flash = require('connect-flash')
+
 
 const app = express();
 const port = 3000;
@@ -15,34 +20,49 @@ app.use(expressLayouts);
 
 // Built-in middleware
 app.use(express.static('publik'));
+app.use(express.urlencoded({extended: true}));
+
+
+//konfigurasi flash
+app.use(cookeiParser('secret'));
+app.use(
+  session({
+    cookie: { maxAge: 6000},
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 
 
 app.get('/', (req, res) => {
-  const mahasiswa = [
-      {
-          nama: 'Ahmad iyasya',
-          email: 'ahiyasya@gmail.com',
-      },
-      {
-          nama: 'Silmi nadira',
-          email: 'silmina@gmail.com',
-      },
-      {
-          nama: 'Silmi iyasya',
-          email: 'silmiiy@gmail.com',
-      },
-  ];
-  res.render('index', {
+
+const mahasiswa = [
+    {
       nama: 'Ahmad iyasya',
-      title: 'Halaman Home',
-      mahasiswa,
-      layout: 'layouts/main-layout', // Perbaikan: layout tanpa ekstensi dan path relatif
+      email: 'ahiyasya@gmail.com',
+  },
+  {
+      nama: 'Silmi nadira',
+      email: 'silmina@gmail.com',
+  },
+  {
+      nama: 'Silmi iyasya',
+      email: 'silmiiy@gmail.com',
+  },
+];
+
+res.render('index', { 
+  nama: 'Ahmad iyasya',
+  title: 'Halaman Home',
+    mahasiswa,
+    layout: 'layouts/main-layout',
   });
 });
 
 app.get('/about', (req, res) => {
-//   res.send('Ini adalah Halaman About');
-// res.sendFile('./about.html', { root: __dirname});
+
 res.render('about', { 
   layout: 'layouts/main-layout',
   title: 'Halaman About'
@@ -51,15 +71,57 @@ res.render('about', {
 
 app.get('/contact', (req, res) => {
   const contacts = loadContact();
-//   res.send('Ini adalah Halaman Contact');
-// res.sendFile('./contact.html', { root: __dirname});
+
 res.render('contact', { 
   layout: 'layouts/main-layout',
   title: 'Halaman Contact',
   contacts,
+  msg: req.flash('msg'),
 });
+});
+//halaman form tambah data contact
+app.get('/contact/add', (req, res) => {
+res.render('add-contact', {
+  title : 'Form Tambah Data Contact',
+  layout : 'layouts/main-layout',
+})
 });
 
+// proses data contact
+app.post('/contact', [
+  body('nama').custom((value) => {
+    const duplikat = cekDuplikat(value);
+    if(duplikat){
+      throw new Error('Nama contact sudah digunakan');
+    }
+    return true;
+  }),
+  check('email', 'Email tidak Valid').isEmail(),
+  
+], (req, res) =>{
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    // return res.status(400).json({ errors: errors.array() });
+    res.render('add-contact',{
+      title: 'Form Tambah Data Contact',
+      layout: 'layouts/main-layout',
+      errors: errors.array(),
+    });
+  }else {
+    
+    addContact(req.body);
+    // kirimkan flash mwssage
+    req.flash('msg', 'Data contact berhasil ditambahkan');
+    res.redirect('/contact');
+  }
+  
+  // res.send(req.body);
+});
+
+
+
+
+// halaman detail contact
 app.get('/contact/:nama', (req, res) => {
   const contact = findContact(req.params.nama);
 res.render('detail', { 
